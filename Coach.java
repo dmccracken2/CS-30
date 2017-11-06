1364cd5767290b5dda4f5b8a20296395a356759e
Furmaan <33426601+Furmaan@users.noreply.github.com>
2017-11-06 09:20:12 -0700

    Add files via upload

---
+++ Coach.java
@@ -0,0 +1,14 @@
+
+package SwimTeam;
+
+public class Coach {
+    protected String First_Name;
+    protected String Last_Name;
+    public int ID;
+//Constructor
+    public Coach(String fn, String ln, int id) {
+        First_Name = fn;
+        First_Name = ln;
+        ID = id;
+    }
+}
